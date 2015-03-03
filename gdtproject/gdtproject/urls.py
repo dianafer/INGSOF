5f33c22851ae8833238ab912c69d69b63a3917b2
didita91 <didif.91@gmail.com>
2015-03-03 11:04:39 -0300

    url_login

--- gdtproject/gdtproject/urls.py
+++ gdtproject/gdtproject/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from apps.form import *
 from apps.models import *
 urlpatterns = patterns('',
     # Examples:
